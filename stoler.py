54b158c9b7251836c1a485209cc6231593a69e40
Magnus Olsen <magnuaol@stud.ntnu.no>
2024-03-11 16:41:47 +0100

    lese seter fra fil

--- stoler.py
+++ stoler.py
@@ -11,12 +11,13 @@ def sette_inn_stoler_gamle_scene(file):
         if line.isalpha(): 
             omraade = line
         else:
+            rad_nr += 1
             for letter in line: 
                 if letter == "x":
                     continue
                 elif letter == 0:
                     seter_nr +=1
-                    rad_nr += 1
+                    
                     #Insert()
                     pass
                 elif letter == 1: 
@@ -25,40 +26,56 @@ def sette_inn_stoler_gamle_scene(file):
                     #Insert()
                 
 def sette_inn_stoler_hovedscenen(file):
-    rad_nr = 1
+    rad_nr = 18
+    sete_nr = 524 + 1
     salnavn = "hovedscenen"
     f = open(file, "r")
     dato = 0
+    omraade = ""
     for line in f:
-        if not dato:
-            dato = line[5:].strip()
         line = line.strip()
-        if line.isalpha(): 
+        if not dato:
+            dato = line[5:]
+        
+        elif line.isalpha(): 
             omraade = line
-            print(omraade)
         else:
+            line = line[::-1]
+            #print(line)
             for letter in line: 
-                sete_nr = 1
                 if letter == "x":
-                    continue
-                elif letter == 0:
-                    sete_nr +=1
-                    rad_nr += 1
+                    print ("yoloman")
+                    sete_nr -= 1
+                    
+                elif letter == "0":
+                    sete_nr -=1
+
+                    if omraade == "Galleri":
+                        print(f"({None}, {sete_nr}, {omraade}, {salnavn}, ikke solgt)")
+
+                    else:
+                        print(f"({rad_nr}, {sete_nr}, {omraade}, {salnavn}, ikke solgt)")
+
+                    #Insert()
+                elif letter == "1":
+
+                    sete_nr -= 1
                     
                     #Insert()
-                    pass
-                elif letter == 1:
-                    sete_nr +=1
-                    rad_nr += 1
-                    #Insert()
-                    print(rad_nr, sete_nr, omraade, salnavn)
+                    if omraade == "Galleri":
+                        print(f"({None}, {sete_nr}, {omraade}, {salnavn}, solgt)")
 
+                    else:
+                        print(f"({rad_nr}, {sete_nr}, {omraade}, {salnavn}, solgt)")
+
+            if omraade != "Galleri":
+                rad_nr -= 1
+            
 sette_inn_stoler_hovedscenen("hovedscenen.txt")
+
+
+
+
+
         
-
-
-
-
-
-
 
