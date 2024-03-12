06d026cf9ab6668e1663929ec1c4170d4ac422eb
EirikSlettan <etsletta@ntnu.no>
2024-03-12 14:00:46 +0100

    La inn data for roller og ansatte for kongsemnene

--- init.bat
+++ init.bat
@@ -2,3 +2,4 @@ echo(----------Fjerner gammel database, lager ny-------------)
 del test.db
 touch test.db
 sqlite3 test.db < sql_sqript_setup_gruppe40.sql
+
---
+++ sql_sqript_insert_gruppe40.sql
@@ -0,0 +1,33 @@
+--Kongsemnene
+INSERT INTO ansatt VALUES (1, 'Arturo Scotti', 'arturo.scotti@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (2, 'Ingunn Beate Strige Øyen', 'ingunn.beate.strige.øyen@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (3, 'Hans Petter Nilsen', 'hans.petter.nilsen@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (4, 'Madeleine Brandtzæg Nilsen', 'madeleine.brandtzæg.nilsen@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (5, 'Synnøve Fossum Eriksen', 'synnøve.fossum.eriksen@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (6, 'Emma Caroline Deichmann', 'emma.caroline.deichmann@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (7, 'Thomas Jensen Takyi', 'thomas.jensen.takyi@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (8, 'Per Bogstad Gulliksen', 'per.bogstad.gulliksen@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (9, 'Isak Holmen Sørensen', 'isak.holmen.sørensen@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (10, 'Fabian Heidelberg Lunde', 'fabian.heidelberg.lunde@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (11, 'Emil Olafsson', 'emil.olafsson@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (12, 'Snorre Ryen Tøndel', 'snorre.ryen.tøndel@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
+INSERT INTO ansatt VALUES (13, 'Yury Butusov', 'yury.butusov@trondelagteater.no', 'False', 'fast ansatt', 'Regi og musikkutvelgelse');
+INSERT INTO ansatt VALUES (14, 'Aleksandr Shishkin-Hokusai', 'aleksandr.shishkin-hokusai@trondelagteater.no', 'False', 'fast ansatt', 'Scenografi og kostymer');
+INSERT INTO ansatt VALUES (15, 'Eivind Myren', 'eivind.myren@trondelagteater.no', 'False', 'fast ansatt', 'Lysdesign');
+INSERT INTO ansatt VALUES (16, 'Mina Rype Stokke', 'mina.rype.stokke@trondelagteater.no', 'False', 'fast ansatt', 'Dramaturg');
+
+INSERT INTO rolle VALUES (1, 'Haakon Haakonson');
+INSERT INTO rolle VALUES (2, 'Inga fra Vartejg (Haakons mor)');
+INSERT INTO rolle VALUES (3, 'Skule jarl');
+INSERT INTO rolle VALUES (4, 'Fru Ragnhild (Skules hustru)');
+INSERT INTO rolle VALUES (5, 'Margrete (Skules datter)');
+INSERT INTO rolle VALUES (6, 'Sigrid (Skules søster) / Ingebjørg');
+INSERT INTO rolle VALUES (7, 'Biskop Nikolas');
+INSERT INTO rolle VALUES (8, 'Gregorius Jonssønn');
+INSERT INTO rolle VALUES (9, 'Paal Flida / Trønder');
+INSERT INTO rolle VALUES (10, 'Baard Bratte / Trønder');
+INSERT INTO rolle VALUES (11, 'Jatgeir Skald / Dagdfinn Bonde');
+INSERT INTO rolle VALUES (12, 'Peter (Prest og Ingebjørgs sønn)');
+
+
+
