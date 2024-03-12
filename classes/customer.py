96b57503a80469388388407d99356a8a8dc9fe82
Einar Therkelsen <einartherkelsen@dhcp-10-22-65-62.wlan.ntnu.no>
2024-03-12 08:38:55 +0100

    Yo

---
+++ classes/customer.py
@@ -0,0 +1,9 @@
+class Customer:
+    def __init__(self, phoneNumber, name, address):
+        self.phoneNumber = phoneNumber
+        self.name = name
+        self.address = address
+
+
+    def buyTicket():
+        return
