e00cd0fdcdaa12e80d806394327a3eaa893d4e4d
EirikSlettan <etsletta@ntnu.no>
2024-03-12 17:59:54 +0100

    La til flere inserts

--- sql_sqript_insert_gruppe40.sql
+++ sql_sqript_insert_gruppe40.sql
@@ -1,4 +1,12 @@
 --Kongsemnene
+INSERT INTO teaterstykke VALUES ('Kongsemnene', 'Henrik Ibsen', 'V2024');
+
+INSERT INTO akt VALUES (1, 'Akt1', 'Kongsemnene');
+INSERT INTO akt VALUES (2, 'Akt2', 'Kongsemnene');
+INSERT INTO akt VALUES (3, 'Akt3', 'Kongsemnene');
+INSERT INTO akt VALUES (4, 'Akt4', 'Kongsemnene');
+INSERT INTO akt VALUES (5, 'Akt5', 'Kongsemnene');
+
 INSERT INTO ansatt VALUES (1, 'Arturo Scotti', 'arturo.scotti@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
 INSERT INTO ansatt VALUES (2, 'Ingunn Beate Strige Øyen', 'ingunn.beate.strige.øyen@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
 INSERT INTO ansatt VALUES (3, 'Hans Petter Nilsen', 'hans.petter.nilsen@trondelagteater.no', 'True', 'fast ansatt', 'skuespiller');
@@ -16,6 +24,23 @@ INSERT INTO ansatt VALUES (14, 'Aleksandr Shishkin-Hokusai', 'aleksandr.shishkin
 INSERT INTO ansatt VALUES (15, 'Eivind Myren', 'eivind.myren@trondelagteater.no', 'False', 'fast ansatt', 'Lysdesign');
 INSERT INTO ansatt VALUES (16, 'Mina Rype Stokke', 'mina.rype.stokke@trondelagteater.no', 'False', 'fast ansatt', 'Dramaturg');
 
+INSERT INTO jobbermed VALUES ('Kongsemnene', 1);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 2);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 3);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 4);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 5);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 6);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 7);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 8);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 9);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 10);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 11);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 12);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 13);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 14);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 15);
+INSERT INTO jobbermed VALUES ('Kongsemnene', 16);
+
 INSERT INTO rolle VALUES (1, 'Haakon Haakonson');
 INSERT INTO rolle VALUES (2, 'Inga fra Vartejg (Haakons mor)');
 INSERT INTO rolle VALUES (3, 'Skule jarl');
@@ -28,6 +53,28 @@ INSERT INTO rolle VALUES (9, 'Paal Flida / Trønder');
 INSERT INTO rolle VALUES (10, 'Baard Bratte / Trønder');
 INSERT INTO rolle VALUES (11, 'Jatgeir Skald / Dagdfinn Bonde');
 INSERT INTO rolle VALUES (12, 'Peter (Prest og Ingebjørgs sønn)');
+
+INSERT INTO harrolle VALUES (1, 1);
+INSERT INTO harrolle VALUES (2, 2);
+INSERT INTO harrolle VALUES (3, 3);
+INSERT INTO harrolle VALUES (4, 4);
+INSERT INTO harrolle VALUES (5, 5);
+INSERT INTO harrolle VALUES (6, 6);
+INSERT INTO harrolle VALUES (7, 7);
+INSERT INTO harrolle VALUES (9, 9);
+INSERT INTO harrolle VALUES (10, 10);
+INSERT INTO harrolle VALUES (11, 11);
+INSERT INTO harrolle VALUES (12, 12);
+
+
+
+
+
+
+
+
+
+
 
 
 
