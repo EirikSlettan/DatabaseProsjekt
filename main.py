a4529b8fb9bc8e4292d2959db72fd951042fa054
Magnus Olsen <magnuaol@stud.ntnu.no>
2024-03-13 21:46:29 +0100

    startet main

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
+from oppg7 import  *
 
 
-def checkForSeats():
-    return
+bredde = 56
+bredde2 = 15
+tegn = "-"
+print("-" *bredde)
+print(f"{tegn*bredde2}VELKOMMEN TIL VÅRT TEATER!{tegn*bredde2}")
+print("-"*bredde)
 
-def buySeats():
-    return
+
+a = input("Ønsker du å gjøre oppgave 7?")
+b = input("hvilken skuespiller?")
+
+if a == "ja":
+
+    find_actor(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- oppg7.py
+++ oppg7.py
@@ -1,9 +1,8 @@
 import sqlite3
-con = sqlite3.connect("test1.db") #Endre dette
-cursor = con.cursor()
 
 def find_actor(navn):
-
+    con = sqlite3.connect("test1.db") #Endre dette
+    cursor = con.cursor()
     cursor.execute('''SELECT DISTINCT a1.navn AS skuespiller1, a2.navn AS skuespiller2, akter.stykkenavn AS skuespill
 FROM ansatt AS a1
 INNER JOIN harrolle AS hr1 ON a1.ansattID = hr1.ansattID
@@ -16,7 +15,10 @@ INNER JOIN harrolle AS hr2 ON r2.rolleID = hr2.rolleID
 INNER JOIN ansatt AS a2 ON hr2.ansattID = a2.ansattID
 WHERE a1.navn != a2.navn AND a1.navn = ?;''', (navn,))
     print(f"Alle skuespillere som deler en akt med {navn} er:")
-    print(cursor.fetchall())
+    
+    for row in cursor.fetchall():
+        print(row)
+
 
 
 find_actor('Sunniva Du Mond Nordal')
