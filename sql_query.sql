ec6a0f77130bd235233fe26a21c0ea9a0df9187c
EirikSlettan <etsletta@ntnu.no>
2024-03-13 13:46:17 +0100

    Lagde query for å finne antall solgte billetter

--- sql_query.sql
+++ sql_query.sql
@@ -24,3 +24,6 @@ INNER JOIN aktharrolle ON akter.aktnr = aktharrolle.aktnr
 INNER JOIN rolle ON rolle.rolleID = aktharrolle.rolleID
 INNER JOIN harrolle ON rolle.rolleID = harrolle.rolleID
 INNER JOIN ansatt ON ansatt.ansattID = harrolle.ansattID
+
+
+ SELECT dato, tid, count(billettID) AS solgt, stykkenavn FROM (forestilling INNER JOIN billett ON forestillingsdato = dato AND forestillingstid = tid AND stykkenavn = teaterstykke) GROUP by teaterstykke, dato, tid  ORDER BY solgt DESC;
