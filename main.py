ad2f8037e179a44b868b1254afd86e0a38f9353a
EirikSlettan <etsletta@ntnu.no>
2024-03-14 20:57:29 +0100

    Lagde funksjoner for å validere input og ryddet i main.py

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
 from Pythonfiler.oppg7 import  *
 from Pythonfiler.finn_forerstillinger_fra_dato import *
 from Pythonfiler.finn_navn_skuespillere import *
+from utils import *
 from Pythonfiler.billetter_solgt import *
 
-
-bredde = 56
-bredde2 = 15
-tegn = "-"
-print("-" *bredde)
-print(f"{tegn*bredde2}VELKOMMEN TIL VÅRT TEATER!{tegn*bredde2}")
-print("-"*bredde)
+velkommen_melding()
 
 #oppgave 1, 2 og 3 kjøres på forhånd med instruksjon fra readme-fil 
 
 #oppgave 4:
-
-dato = input("Hvilken dato ønsker du å sjekke antall solgte billetter til alle forestillingene som vises denne dagen? Svar på formatet: (YYYY-MM-DD): ")
+melding = "Hvilken dato ønsker du å sjekke antall solgte billetter til alle forestillingene som vises denne dagen? Svar på formatet: (YYYY-MM-DD): "
+dato = input(melding)
+while not validate_date(dato):
+    print("Ikke en gyldig dato")
+    dato = input(melding)
 finn_forestillinger(dato)
 
 #oppgave 5
-svar5 = input("Ønsker du å se oppgave5 (Y/N)?: ").upper()
-
+melding = "Ønsker du å se oppgave5 (Y/N)?: "
+svar5 = input(melding).upper()
+svar5 = validate_input(svar5, melding )
 if svar5 == "Y":
     finn_navn_skuespillere()
 else:
     print("Det var synd, vi har jobbet masse med den :(")
 
-
 #oppgave 6
-svar6 = input("Ønsker du å se oppgave6 (Y/N)?: ").upper()
-
+melding = "Ønsker du å se oppgave6 (Y/N)?: "
+svar6 = input(melding).upper()
+svar6 = validate_input(svar6, melding)
 if svar6 == "Y":
     finn_billetter_solgt()
 else:
     print("Hæ? Anbefaler å se den!")
 
 #oppgave 7
-
-svar7 = input("Ønsker du å se oppgave7 (Y/N)?: ").upper()
-
+melding = "Ønsker du å se oppgave7 (Y/N)?: "
+svar7 = input(melding).upper()
+svar7 = validate_input(svar7, melding)
 if svar7 == "Y":
     skuespiller = input("Yes! Hvilken skuespiller vil du se ALLE skuespillerkollegaene til? ")
     find_actor(skuespiller)
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
-def find_longest_entry(data, column_names): #Finds longest entry in each column
+from re import *
+
+def find_longest_entry(data, column_names): #Finner input med flest tegn i hver kolonne, returnerer en list
     longest = [0]*len(column_names)
     for i in range(len(data)):
         row = data[i]
@@ -10,7 +12,7 @@ def find_longest_entry(data, column_names): #Finds longest entry in each column
             longest[i]= len(column_names[i])
     return longest
 
-def create_table(data, column_names):
+def create_table(data, column_names): #Lager en tabell lik den i sqlite3 .mode table, tar inn cursor.fetchall()
     dig = find_longest_entry(data, column_names)
     entry_lengths = [x+1 for x in dig]
     table_length = sum(entry_lengths) + len(column_names)+1
@@ -21,8 +23,6 @@ def create_table(data, column_names):
     for i in range(len(column_names)):
         formatter += "{" + f":<{entry_lengths[i]}" + "}|"
         columns += "{" + f":^{entry_lengths[i]}" + "}|"
-    
-    
     index = 0
     for length in entry_lengths:
         border = border[:index] + "+" + border[index:]
@@ -35,3 +35,26 @@ def create_table(data, column_names):
     for row in data: 
         print(formatter.format(*row))
     print(border)
+
+def velkommen_melding():
+    bredde = 56
+    bredde2 = 14
+    tegn = "-"
+    print(tegn *bredde)
+    print(tegn *bredde)
+    print(f"{tegn*bredde2} VELKOMMEN TIL VÅRT TEATER! {tegn*bredde2}")
+    print(tegn*bredde)
+    print(tegn*bredde)
+
+def validate_input(svar, melding):
+    while True:
+        if svar.upper() == "Y" or svar.upper() == "N":
+            return svar
+        print("Ugyldig svar, prøv igjen")
+        svar = input(melding).upper()
+        
+def validate_date(dato):
+    gyldig_input = r'^\d{4}-\d{2}-\d{2}$'
+    if match(gyldig_input, dato):
+        return True
+    False 
