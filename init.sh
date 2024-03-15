612b533dbbf213d0a1e11fc98a57b73082818357
EirikSlettan <etsletta@ntnu.no>
2024-03-15 11:31:32 +0100

    Oppdaterte gitignore og shell-script

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-rm database.db
-touch database.db
-sqlite3 database.db < sql_sqript_setup_gruppe40.sql
-sqlite3 database.db < sql_sqript_insert_gruppe40.sql
-python3 stoler.py
+rm teater_database.db
+touch teater_database.db
+sqlite3 teater_database.db < sqlfiler/schema.sql
+sqlite3 database.db < sqlfiler/insert_known_values.sql
+python3 pythonfiler/lese_fra_fil.py
